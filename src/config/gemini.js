c7c9a1568d33e17182a0a5b3fbfad1d88dc75e08
Luarbtw <jaum6011@gmail.com>
2026-05-04 21:27:29 -0400

    gemini 3.0

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY); // Le variaveis do .env
 
 const model = genAI.getGenerativeModel({
-    model: 'gemini-2.5-flash'
+    model: 'gemini-3.0-flash'
 }); // seleciona o modelo IA
 
-module.exports = model;
+module.exports = model;     
